09e4a7e31e0cc4ce5885e668bc77756db9fb4b52
ralph950412 <ralph950412@gmail.com>
2026-07-15 13:58:56 +0000

    avicii: Bump libwfdservice to android.media.audio.common.types-V5
    Change-Id: I082fcd534af020d8240d028bc58fe4cba2994750

--- extract-files.py
+++ extract-files.py
@@ -107,6 +107,8 @@ blob_fixups: blob_fixups_user_type = {
         .remove_needed('libheif.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'vendor/etc/libnfc-nci.conf': blob_fixup()
         .regex_replace('NFC_DEBUG_ENABLED=0x01', 'NFC_DEBUG_ENABLED=0x00'),
     'vendor/etc/libnfc-nxp.conf': blob_fixup()
